7bf5ee391cbfb9a2854be9d01d6287d0dc5ada10
AlexBruch <alexbruchforrellad@gmail.com>
2014-10-17 17:29:40 +0200

    Bug resolt

--- Poligon/src/Poligon/Poligon.java
+++ Poligon/src/Poligon/Poligon.java
@@ -5,8 +5,14 @@ public abstract class Poligon implements Comparable<Poligon>{
 	public abstract double calcularArea();
 
 	@Override
-	public int compareTo(Poligon o) {
-		// Comparem les àrees
-		return (int) (this.calcularArea() - o.calcularArea());
+	public int compareTo(Poligon p) {
+		
+		double miArea = this.calcularArea();
+		double pArea = p.calcularArea();
+		
+		if(miArea<pArea) return -1;
+		if(miArea>pArea) return 1;
+		
+		return 0;
 	}
 }
